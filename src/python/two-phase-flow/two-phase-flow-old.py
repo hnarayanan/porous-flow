b4627d670eb5f9d321026bb2bcc667ecb1078253
Harish Narayanan <harish@simula.no>
2010-01-28 12:44:28 +0000

    Renamed files

---
+++ src/python/two-phase-flow/two-phase-flow-old.py
@@ -0,0 +1,140 @@
+""" . 
+"""
+
+__author__    = "Garth N. Wells (gnw20@cam.ac.uk)"
+__date__      = "2009-10-28"
+__copyright__ = "Copyright (C) 2009 Garth N. Wells"
+__license__   = "GNU GPL Version 3.0"
+
+from dolfin import *
+
+# Optimize compilation of the form
+parameters.optimize = False
+
+# Sub domain for inflow (right)
+class Inflow(SubDomain):
+    def inside(self, x, on_boundary):
+        return x[0] < DOLFIN_EPS and on_boundary
+
+# Pressure boundary condition
+class Gp(Expression):
+    def eval(self, values, x):
+        values[0] = 1.0 - x[0] 
+
+# Saturation boundary condition
+class Gsw(Expression):
+    def eval(self, values, x):
+        if x[0] < DOLFIN_EPS:
+            values[0] =  0.3
+
+# Velocity boundary condition
+class Velocity(Expression):
+    def eval(self, values, x):
+        values[0] =  1.0
+        values[1] =  0.0
+
+# Define boundary of domain
+class Boundary(SubDomain):
+    def inside(self, x, on_boundary):
+        return on_boundary
+
+mesh = UnitSquare(16, 16)
+n = FacetNormal(mesh)
+
+# Create mesh function over the cell facets
+sub_domains = MeshFunction("uint", mesh, mesh.topology().dim() - 1)
+sub_domains.set_all(3)
+inflow = Inflow()
+inflow.mark(sub_domains, 1)
+
+# Create function spaces
+Pv1 = VectorFunctionSpace(mesh, "Lagrange", 1)
+BDM = FunctionSpace(mesh, "BDM", 1)
+P0  = FunctionSpace(mesh, "DG", 0)
+P1  = FunctionSpace(mesh, "CG", 1)
+
+mixed_space = MixedFunctionSpace([BDM, P0, P1])
+
+# Boundary function
+gsw = Gsw(degree=1)
+gp = Gp(degree=1)
+
+dt = 0.01
+f = 0.0
+k = 1.0
+mobility = 1.0
+
+V   = TestFunction(mixed_space)
+dU  = TrialFunction(mixed_space)
+U   = Function(mixed_space)
+U0  = Function(mixed_space)
+
+w, q, v    = split(V)
+u, p, s    = split(U)
+u0, p0, s0 = split(U0)
+
+## Darcy equations
+
+# Mass conservation eqn
+L_darcy_mass = q*div(u)*dx - q*f*dx
+a_darcy_mass = derivative(L_darcy_mass, U, dU)
+
+# Pressure eqn
+L_darcy_p = (1.0/k)*(1.0/mobility)*dot(w, u)*dx - div(w)*p*dx \
+           + dot(w, n)*gp*ds
+a_darcy_p = derivative(L_darcy_p, U, dU)
+
+L_darcy = L_darcy_mass + L_darcy_p
+a_darcy = a_darcy_mass + a_darcy_p
+
+# Upwind normal velocity: ( dot(v, n) + |dot(v, n)| )/2.0 
+# (using velocity from previous step on facets)
+un   = (dot(u0, n) + sqrt(dot(u0, n)*dot(u0, n)))/2.0
+un_h = (dot(u0, n) - sqrt(dot(u0, n)*dot(u0, n)))/2.0
+
+### Saturation equation
+
+# Mid-point value of saturation
+s_mid = 0.5*(s + s0)
+
+# Fractional flow function
+F = s_mid*s_mid/( s_mid*s_mid + 0.2*(1.0-s_mid)*(1.0-s_mid) );
+
+# Forms
+
+# DG form
+#L_s =  dot(v, s)*dx - dot(v, s0)*dx - dt*dot(grad(v), u*F)*dx \
+#      + dt*dot(jump(v), un('+')*F('+') - un('-')*F('-'))*dS \
+#      + dt*dot(v, un*F)*ds + dt*dot(v, un_h*gsw)*ds
+
+# CG form (crude with artificial diffusion)
+L_s =  dot(v, s)*dx - dot(v, s0)*dx - dt*dot(grad(v), u*F)*dx \
+      + dt*dot(grad(v), grad(s_mid))*dx \
+      + dt*dot(v, un*F)*ds + dt*dot(v, un_h*gsw)*ds
+
+a_s = derivative(L_s, U, dU)
+
+# Sum forms
+L = L_darcy + L_s
+a = a_darcy + a_s
+
+pde= VariationalProblem(a, L, nonlinear=True)
+
+fs = File("results/s_phase.pvd")
+fu = File("results/u.pvd")
+
+t = 0.0
+T = 50*dt
+while t < T:
+    t += dt
+    U0.assign(U)
+    pde.solve(U)
+    u, p, s = U.split() 
+    fs << s
+    uh = project(u, Pv1)
+    fu << uh
+
+uh = project(u, Pv1)
+plot(uh)
+interactive() 
+
--- src/python/two-phase-flow/two-phase-flow.py
+++ src/python/two-phase-flow/two-phase-flow.py
@@ -1,140 +1,182 @@
-""" . 
+"""
+This program solves pressure-driven, time-dependent flow of two phases
+through porous media.
+
+Strong form:
+
+    (lambda(s)*K)^(-1)*u + grad(p) = 0
+                            div(u) = 0
+              ds/dt + u.grad(F(s)) = 0,
+
+where,
+
+    lambda(s) = 1.0/mu_rel*s^2 + (1 - s)^2
+         F(s) = k_rw(s)/mu_w/(k_rw(s)/mu_w + k_ro(s)/mu_o)
+              = s^2/(s^2 + mu_rel*(1 - s)^2).
+
+
+One can then can post-calculate the velocity of each phase using the
+relation: u_j = - (k_rj(s)/mu_j)*K*grad(p).
+
+Weak form:
+
+Find u, p, s in V, such that,
+
+   (v, (lambda*K)^(-1)*u) - (div(v), p) = - (v, pbar*n)_N       (1)
+      (q, div(u)) = 0 or - (grad(q), u) = - (q, ubar.n)_N       (2)
+            (r, ds/dt) - (grad(r), F*u) = - (r, F(sbar)*u.n)_N  (3)
+                             
+for all v, q, r in V'.
+
+Model problem:
+
+ |----4----|
+ |         |
+ 1         2
+ |         |
+ |----3----|
+
+Initial Conditions:
+u(x, 0) = 0
+p(x, 0) = 0
+s(x, 0) = 0 in \Omega
+
+Boundary Conditions:
+p(x, t) = 1 - x on \Gamma_{1, 2, 3, 4}
+s(x, t) = 1 on \Gamma_1 if u.n < 0
+s(x, t) = 0 on \Gamma_{2, 3, 4} if u.n < 0
+
+Parameters:
+mu_rel, Kinv, lmbdainv, F, dt, T
+
+This implementation includes functional forms from the deal.II demo
+available at: http://www.dealii.org/6.2.1/doxygen/deal.II/step_21.html
 """
 
-__author__    = "Garth N. Wells (gnw20@cam.ac.uk)"
-__date__      = "2009-10-28"
-__copyright__ = "Copyright (C) 2009 Garth N. Wells"
+__author__    = "Garth N. Wells (gnw20@cam.ac.uk) and Harish Narayanan (harish@simula.no)"
+__date__      = "2010-01-26"
+__copyright__ = "Copyright (C) 2010 Garth N. Wells and Harish Narayanan"
 __license__   = "GNU GPL Version 3.0"
 
 from dolfin import *
+parameters.optimize=True
 
-# Optimize compilation of the form
-parameters.optimize = False
+# Computational domain and geometry information
+mesh = UnitSquare(40, 40)
+n = FacetNormal(mesh)
+boundary = MeshFunction("uint", mesh, mesh.topology().dim() - 1)
+boundary.set_all(5)
+left, right, bottom, top = compile_subdomains(["x[0] == 0.0", "x[0] == 1.0", "x[1] == 0.0", "x[1] == 1.0"])
+left.mark(boundary, 1)
+right.mark(boundary, 2)
+bottom.mark(boundary, 3)
+top.mark(boundary, 4)
 
-# Sub domain for inflow (right)
-class Inflow(SubDomain):
-    def inside(self, x, on_boundary):
-        return x[0] < DOLFIN_EPS and on_boundary
+# Physical parameters, functional forms and boundary conditions
+# Relative viscosity of water w.r.t. crude oil
+mu_rel = 0.2
+
+# Spatially-varying permeability matrix (inverse)
+kinv = Expression("1.0/std::max(exp(-pow((x[1] - 0.5 - 0.1*sin(10*x[0]))/0.1, 2.0)), 0.01)")
+zero = Expression("0.0")
+Kinv = as_matrix(((kinv, zero), (zero, kinv)))
+
+# Total mobility
+def lmbdainv(s):
+    return 1.0/((1.0/mu_rel)*s**2 + (1 - s)**2)
+
+# Fractional flow function
+def F(s):
+    return s**2/(s**2 + mu_rel*(1 - s)**2)
+
+# Time step
+dt = 0.001
 
 # Pressure boundary condition
-class Gp(Expression):
+class PressureBC(Expression):
     def eval(self, values, x):
-        values[0] = 1.0 - x[0] 
+        values[0] = 1.0 - x[0]
 
 # Saturation boundary condition
-class Gsw(Expression):
+class SaturationBC(Expression):
     def eval(self, values, x):
-        if x[0] < DOLFIN_EPS:
-            values[0] =  0.3
+        values[0] = 1.0 - x[0]
 
-# Velocity boundary condition
-class Velocity(Expression):
+# Normal velocity boundary condition
+class NormalVelocityBC(Expression):
     def eval(self, values, x):
-        values[0] =  1.0
-        values[1] =  0.0
+        values[0] = 0.0
 
-# Define boundary of domain
-class Boundary(SubDomain):
-    def inside(self, x, on_boundary):
-        return on_boundary
+# Function spaces
+degree = 1
+BDM = FunctionSpace(mesh, "BDM", degree)
+DG  = FunctionSpace(mesh, "DG",  degree - 1)
+# CG  = FunctionSpace(mesh, "CG",  degree)
+mixed_space = MixedFunctionSpace([BDM, DG, DG]) #FIXME: Using DG for the saturation
+# For projecting fields
+P1s  = FunctionSpace(mesh, "CG",  1)
+P1v  = VectorFunctionSpace(mesh, "CG",  1)
 
-mesh = UnitSquare(16, 16)
-n = FacetNormal(mesh)
 
-# Create mesh function over the cell facets
-sub_domains = MeshFunction("uint", mesh, mesh.topology().dim() - 1)
-sub_domains.set_all(3)
-inflow = Inflow()
-inflow.mark(sub_domains, 1)
-
-# Create function spaces
-Pv1 = VectorFunctionSpace(mesh, "Lagrange", 1)
-BDM = FunctionSpace(mesh, "BDM", 1)
-P0  = FunctionSpace(mesh, "DG", 0)
-P1  = FunctionSpace(mesh, "CG", 1)
-
-mixed_space = MixedFunctionSpace([BDM, P0, P1])
-
-# Boundary function
-gsw = Gsw(degree=1)
-gp = Gp(degree=1)
-
-dt = 0.01
-f = 0.0
-k = 1.0
-mobility = 1.0
-
+# Functions
 V   = TestFunction(mixed_space)
 dU  = TrialFunction(mixed_space)
 U   = Function(mixed_space)
 U0  = Function(mixed_space)
 
-w, q, v    = split(V)
-u, p, s    = split(U)
+v, q, r = split(V)
+u, p, s = split(U)
 u0, p0, s0 = split(U0)
 
-## Darcy equations
+pbar = PressureBC()
+sbar = SaturationBC()
+unbar = NormalVelocityBC()
 
-# Mass conservation eqn
-L_darcy_mass = q*div(u)*dx - q*f*dx
-a_darcy_mass = derivative(L_darcy_mass, U, dU)
+s_mid = 0.5*(s0 + s)
 
-# Pressure eqn
-L_darcy_p = (1.0/k)*(1.0/mobility)*dot(w, u)*dx - div(w)*p*dx \
-           + dot(w, n)*gp*ds
-a_darcy_p = derivative(L_darcy_p, U, dU)
+# Variational forms and problem
+L1 = inner(v, lmbdainv(s_mid)*Kinv*u)*dx - div(v)*p*dx + inner(v, pbar*n)*ds(1) \
+    + inner(v, pbar*n)*ds(2) + inner(v, pbar*n)*ds(3) + inner(v, pbar*n)*ds(4)
+a1 = derivative(L1, U, dU)
 
-L_darcy = L_darcy_mass + L_darcy_p
-a_darcy = a_darcy_mass + a_darcy_p
+L2 = q*div(u)*dx
+a2 = derivative(L2, U, dU)
 
-# Upwind normal velocity: ( dot(v, n) + |dot(v, n)| )/2.0 
+# Upwind normal velocity: (dot(v, n) + |dot(v, n)|)/2.0 
 # (using velocity from previous step on facets)
 un   = (dot(u0, n) + sqrt(dot(u0, n)*dot(u0, n)))/2.0
 un_h = (dot(u0, n) - sqrt(dot(u0, n)*dot(u0, n)))/2.0
+stabilisation = dt*inner(jump(r), un('+')*F(s_mid)('+') - un('-')*F(s_mid)('-'))*dS \
+    + dt*inner(r, un_h*sbar)*ds
 
-### Saturation equation
+L3 = r*(s - s0)*dx - dt*inner(grad(r), F(s_mid)*u)*dx + dt*r*inner(F(sbar)*u, n)*ds(1) \
+    + stabilisation
+a3 = derivative(L3, U, dU)
 
-# Mid-point value of saturation
-s_mid = 0.5*(s + s0)
+L = L1 + L2 + L3
+a = a1 + a2 + a3
 
-# Fractional flow function
-F = s_mid*s_mid/( s_mid*s_mid + 0.2*(1.0-s_mid)*(1.0-s_mid) );
+problem = VariationalProblem(a, L, exterior_facet_domains=boundary, nonlinear=True)
+problem.parameters["newton_solver"]["absolute_tolerance"] = 1e-12 
+problem.parameters["newton_solver"]["relative_tolerance"] = 1e-16
+problem.parameters["newton_solver"]["maximum_iterations"] = 100
 
-# Forms
-
-# DG form
-#L_s =  dot(v, s)*dx - dot(v, s0)*dx - dt*dot(grad(v), u*F)*dx \
-#      + dt*dot(jump(v), un('+')*F('+') - un('-')*F('-'))*dS \
-#      + dt*dot(v, un*F)*ds + dt*dot(v, un_h*gsw)*ds
-
-# CG form (crude with artificial diffusion)
-L_s =  dot(v, s)*dx - dot(v, s0)*dx - dt*dot(grad(v), u*F)*dx \
-      + dt*dot(grad(v), grad(s_mid))*dx \
-      + dt*dot(v, un*F)*ds + dt*dot(v, un_h*gsw)*ds
-
-a_s = derivative(L_s, U, dU)
-
-# Sum forms
-L = L_darcy + L_s
-a = a_darcy + a_s
-
-pde= VariationalProblem(a, L, nonlinear=True)
-
-fs = File("results/s_phase.pvd")
-fu = File("results/u.pvd")
+u_file = File("velocity.pvd")
+p_file = File("pressure.pvd")
+s_file = File("saturation.pvd")
 
 t = 0.0
-T = 50*dt
+T = 1000*dt
 while t < T:
     t += dt
     U0.assign(U)
-    pde.solve(U)
-    u, p, s = U.split() 
-    fs << s
-    uh = project(u, Pv1)
-    fu << uh
-
-uh = project(u, Pv1)
-plot(uh)
-interactive() 
-
+    problem.solve(U)
+    u, p, s = U.split()
+    uh = project(u, P1v)
+    sh = project(s, P1s)
+#    plot(uh, title="Velocity")
+#    plot(p, title="Pressure")
+#    plot(s, title="Saturation")
+    u_file << uh
+    p_file << p
+    s_file << sh
