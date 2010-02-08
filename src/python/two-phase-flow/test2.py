3296f0199b9b53eeac5701addfca17176d02a9ae
Garth N. Wells <gnw20@cam.ac.uk>
2010-02-08 08:34:48 +0000

    Make problem non-mixed.

--- src/python/two-phase-flow/test2.py
+++ src/python/two-phase-flow/test2.py
@@ -7,16 +7,13 @@ mesh_init = UnitSquare(2, 2, "crossed")
 mesh_new = Mesh(mesh_init)
 
 V0 = FunctionSpace(mesh_init, "CG", 1)
-ME0 = V0 + V0
+ME0 = V0
 U0 = Function(ME0)
-#u0 = Function(V0)
-#p0 = Function(V0)
-#s0 = Function(V0)
 
-#BDM0 = FunctionSpace(mesh_init, "Brezzi-Douglas-Marini", 1)
-#DG0 = FunctionSpace(mesh_init, "Discontinuous Lagrange", 0)
-#ME0 = MixedFunctionSpace([BDM0, DG0, DG0])
+# Mixed
+#ME0 = V0 + V0
 #U0 = Function(ME0)
+#u0, p0 = split(U0)
 
 t  = 0.0
 dt = Constant(0.005)
@@ -29,29 +26,21 @@ while t < T:
 
         mesh = mesh_new
 
-        #BDM = FunctionSpace(mesh, "Brezzi-Douglas-Marini", 1)
-        #DG = FunctionSpace(mesh, "Discontinuous Lagrange", 0)
-        #ME = MixedFunctionSpace([BDM, DG, DG])
-
         V  = FunctionSpace(mesh, "CG", 1)
-        ME = V + V
-        ME = MixedFunctionSpace([V, V])
 
-        (v, q) = TestFunctions(ME)
-        dU     = TrialFunction(ME)
-        U      = Function(ME)
+        ME = V
+        v  = TestFunction(ME)
+        dU = TrialFunction(ME)
+        U  = Function(ME)
 
-        #(v, q, r) = TestFunctions(ME)
-        #dU        = TrialFunction(ME)
-        #U         = Function(ME)
+        #ME = MixedFunctionSpace([V, V])
+        #(v, q) = TestFunctions(ME)
+        #dU     = TrialFunction(ME)
+        #U      = Function(ME)
 
-        #u0,  p0, s0  = split(U0)
-        #u,   p, s    = split(U)
-        #du, dp, ds   = split(dU)
-
-        u0,  p0 = split(U0)
-        u,   p  = split(U)
-        du, dp  = split(dU)
+        #u0,  p0 = split(U0)
+        #u,   p  = split(U)
+        #du, dp  = split(dU)
 
         print "Verify function dim (a)"
         print U.function_space().mesh().num_cells()
@@ -59,7 +48,8 @@ while t < T:
         print U0.function_space().mesh().num_cells()
         print "End verify functions dims"
   
-        L = dot(v, u-u0)*dx + q*(p-p0)*dx #+ r*(s-s0)*dx
+        #L = dot(v, u-u0)*dx + q*(p-p0)*dx
+        L = v*(U-U0)*dx
         a = derivative(L, U, dU)
 
         pde = VariationalProblem(a, L)
@@ -68,12 +58,17 @@ while t < T:
         mesh_new = Mesh(mesh)
         mesh_new.refine()
 
-    #U0 = U
-    V1 = FunctionSpace(mesh_new, "CG", 1)
-    ME1 = V1 + V1
-    U0 = Function(ME1)
+    # This breaks for mixed element
+    U0 = U
+
+    # This works for mixed element
+    #V1 = FunctionSpace(mesh_new, "CG", 1)
+    #ME1 = V1 + V1
+    #U0 = Function(ME1)
+    #U0.interpolate(U)
+
     #U0 = Function(ME)
-    U0.interpolate(U)
+    #U0.interpolate(U)
 
     raw_input("Check memory use and press ENTER to continue")
 
